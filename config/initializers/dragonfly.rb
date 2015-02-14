aedf1605351f7c0e58a07a1b6329c572ab965070
Naveen <none.wiseguy@gmail.com>
2015-02-14 01:35:05 -0700

    Recommit

---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,26 @@
+require 'dragonfly'
+
+# Configure
+Dragonfly.app.configure do
+  plugin :imagemagick
+
+  secret "1c30efd6cad046f28dffcc2163c6ce6029e25b84dd313b2689c751dc706d8538"
+
+  url_format "/media/:job/:name"
+
+  datastore :file,
+    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+    server_root: Rails.root.join('public')
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware
+
+# Add model functionality
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.extend Dragonfly::Model
+  ActiveRecord::Base.extend Dragonfly::Model::Validations
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Images::Application.config.secret_key_base = 'ff3d9f4efbbcacd6f704502500826c7f2406099f8dd4cce2b04fdf7d22163c93185f74229d0a5b0cc232d2f9296d4d881c1dc7bcbe841f9d393aaa07ba19c023'
